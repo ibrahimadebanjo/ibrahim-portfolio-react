407c6fde7557e7494182c83521ad1b08b06c786b
Faatimah <adebanjofatimo2000@gmail.com>
2024-11-29 17:05:15 +0100

    updated hero in trying github.io

---
+++ src/Pages/About.jsx
@@ -0,0 +1,10 @@
+import heroImage from "../assets/log-in-girl.svg"
+const About = () => {
+  return (
+    <div>
+      <img className="w-[600px] bg-green- h-full" src={heroImage} alt="" />
+    </div>
+  )
+}
+
+export default About
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+import image from "../assets/Questions-bro.svg"
+const Contact = () => {
+  return (
+    <div className="flex">
+      <img className="w-[580px]" src={image} alt="" />
+      <div className="w-[580px] h-[580px] bg-green-100">
+
+      </div>
+    </div>
+  )
+}
+
+export default Contact
